39c56ce113ff69fb03e2093d956280e34cd25c15
Nate Richardson <nathanjrichardson3@gmail.com>
2017-12-28 23:01:53 -0600

    BuilderContoller initialized/early implementation testing

--- src/main/java/org/launchcode/quizbuilder/controllers/BuilderController.java
+++ src/main/java/org/launchcode/quizbuilder/controllers/BuilderController.java
@@ -1,12 +1,15 @@
 package org.launchcode.quizbuilder.controllers;
 
 
+import org.launchcode.quizbuilder.models.QuestionInterface;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("builder")
@@ -19,6 +22,8 @@ public class BuilderController {
     }
 
     @RequestMapping(value="add", method = RequestMethod.POST)
-    public String processAddForm(@RequestParam String qType, String qAnswer, Array)
+    public String processAddForm(@ModelAttribute @Valid QuestionInterface newQuestion,
+                                 @RequestParam String qType) {
+        `
     }
 }
